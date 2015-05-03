7606cb6f52b7350decc0efc2b3e685e6b32dd89e
Avner Peled <avnerus@gmail.com>
2015-05-03 14:59:27 +0300

    no more server store

--- src/app/stores.js
+++ src/app/stores.js
@@ -1,12 +1,10 @@
 import FruitStore from './stores/fruit';
-import ServerStore from './stores/server';
 import AuthStore from './stores/auth';
 import MainStore from './stores/main';
 
 export default class Stores {
     constructor() {
         this.fruit = new FruitStore();
-        this.server = new ServerStore();
         this.auth = new AuthStore();
         this.main = new MainStore();
     }
