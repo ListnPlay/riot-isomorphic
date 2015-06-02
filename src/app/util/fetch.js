4779c49eac49b53f7352dfb5e502b030482d242b
Avner Peled <avnerus@gmail.com>
2015-06-02 16:53:36 +0300

    An improved fetch util

--- src/app/util/fetch.js
+++ src/app/util/fetch.js
@@ -1,13 +1,15 @@
 // Fetch Util
-import fetch from 'isomorphic-fetch';
+import fetch_ from 'isomorphic-fetch';
 
 class FetchUtil {
     constructor() {
+        this.fetch = fetch_.bind(undefined); // this solves an invocation error problem in chrome, according to https://github.com/matthew-andrews/isomorphic-fetch/pull/20
     }
 
     postJSON(target, data) {
-        return fetch(target, {
-            method: 'POST', 
+        return this.fetch(target, {
+            method: 'POST',
+            credentials: 'include',
             body: JSON.stringify(data),
             headers: {
                     'Accept': 'application/json',
@@ -15,10 +17,19 @@ class FetchUtil {
             }
         });
     }
+
+    getJSON(target) {
+        return this.fetch(target, {
+            method: 'GET',
+            credentials: 'include',
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+    }
+
 };
 
 // Singleton
 let instance = new FetchUtil();
 export default instance;
-
-
